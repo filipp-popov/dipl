20c0184cf2d0e02d3a52880c7b7d2bb121e7d0b0
philippopov <philippopov@hotmail.com>
2023-06-02 22:13:27 +0300

    models

--- dip/main/admin.py
+++ dip/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Location, Event, Review
 
+admin.site.register(Location)
+admin.site.register(Event)
+admin.site.register(Review)
 # Register your models here.
--- dip/main/models.py
+++ dip/main/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Location(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.title
+    
+class Event(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    slug = models.SlugField()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    # title = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    # slug = models.SlugField()
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.event.title
+
